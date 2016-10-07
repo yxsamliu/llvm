6c755a02cb0cf6fee676fb84146b6f7e70532b19
Wen-Heng (Jack) Chung <whchung@gmail.com>
2016-10-07 15:55:24 +0800

    [Promote] remove obsolete code

--- lib/Transforms/Promote/Promote.cpp
+++ lib/Transforms/Promote/Promote.cpp
@@ -1968,19 +1968,6 @@ bool PromoteGlobals::runOnModule(Module& M)
         }
         updateKernels (M, promotedKernels);
 
-        /// FIXME: The following code can be removed. It is too late to add
-        ///        NoDuplicate attribute on barrier in Promote pass. We already
-        //         add NoDuplicate attribute in clang
-#if 0
-        // If the barrier present is used, we need to ensure it cannot be duplicated.
-        for (Module::iterator F = M.begin(), Fe = M.end(); F != Fe; ++F) {
-                StringRef name = F->getName();
-                if (name.equals ("barrier")) {
-                        F->addFnAttr (Attribute::NoDuplicate);
-                }
-        }
-#endif
-
         // Rename local variables per SPIR naming rule
         Module::GlobalListType &globals = M.getGlobalList();
         for (Module::global_iterator I = globals.begin(), E = globals.end();
